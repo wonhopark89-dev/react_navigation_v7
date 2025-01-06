31692f5ebb8321852103f107230d61a2f5a2ba0e
wonhopark89-dev <wonhopark89.dev@gmail.com>
2025-01-07 00:15:48 +0900

    패키지 추가

---
+++ src/RootScreen.tsx
@@ -0,0 +1,15 @@
+/**
+ * created by : wonhopark89@gmail.com
+ * created at : 2025/01/07 00:13:49
+ */
+import {View, Text} from 'react-native';
+
+const RootScreen = () => {
+  return (
+    <View>
+      <Text>RootScreen</Text>
+    </View>
+  );
+};
+
+export default RootScreen;
